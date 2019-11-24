695d40cfd756c2e402c962b07062fddc39984082
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-24 20:20:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      if 
+    elsif req.path.
       
       
       
