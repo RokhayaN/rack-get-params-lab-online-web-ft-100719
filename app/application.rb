ec44053732039cac6c8887612f08cf63b0c560a2
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-24 20:39:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,15 +28,11 @@ class Application
       @@cart << search_term
         resp.write "added #{search_term}"
       else
-        resp.write ""
-    
-      
-      
-      
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
